af89550d780af774b7e60860c018aea6d0eb824a
Avinash174 <avinashmagar15@gmail.com>
2024-05-09 09:58:32 +0530

    new changes add

--- lib/gradient_color.dart
+++ lib/gradient_color.dart
@@ -24,17 +24,31 @@ class _GradientColorTryState extends State<GradientColorTry> {
         ),
         child: Center(
           child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Image.asset(
                 'assets/images/dice-3.png',
                 width: 200,
               ),
+              const SizedBox(
+                height: 20,
+              ),
               TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Roll Dice',
-                    style: TextStyle(color: Colors.yellowAccent),
-                  ))
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  // padding: const EdgeInsets.only(
+                  //   top: 20,
+                  // ),
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                child: const Text(
+                  'Roll Dice',
+                ),
+              )
             ],
           ),
         ));
